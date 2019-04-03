09e9de0fdcaf9eb4159a9098c00c52db8ad8a25c
Spencer Adolph <c19spencer.adolph@usafa.edu>
2019-04-03 14:27:35 +0000

    fixed into and out of containers

--- backend/game/pieces/pieceMove.php
+++ backend/game/pieces/pieceMove.php
@@ -83,7 +83,7 @@ if ($placementUnitId != 15 && $placementCurrentMoves == 0) {
     exit;
 }
 if ($placementUnitId != 15) {
-    if ($_SESSION['dist'][$oldPositionId][$newPositionId] != 1 && ($oldPositionId != $newPositionId)) {
+    if (($_SESSION['dist'][$oldPositionId][$newPositionId] != 1 && ($oldPositionId != $newPositionId)) && $newPositionId != -1) {
         echo "Can only move 1 space at a time.";
         exit;
     }
