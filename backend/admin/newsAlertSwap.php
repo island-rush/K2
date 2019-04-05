2f496ebe0da13f8741536887e97fc623b93607db
spenceradolph <c19spencer.adolph@usafa.edu>
2019-04-05 08:15:11 -0600

    cleaned up news alert swap

--- backend/admin/newsAlertSwap.php
+++ backend/admin/newsAlertSwap.php
@@ -14,12 +14,8 @@ $query = "UPDATE newsAlerts SET newsOrder = ? WHERE newsGameId = ? AND newsOrder
 $preparedQuery = $db->prepare($query);
 $preparedQuery->bind_param("iii", $tempOrder, $gameId, $old1order);
 $preparedQuery->execute();
-$query = "UPDATE newsAlerts SET newsOrder = ? WHERE newsGameId = ? AND newsOrder = ?";
-$preparedQuery = $db->prepare($query);
 $preparedQuery->bind_param("iii", $old1order, $gameId, $old2order);
 $preparedQuery->execute();
-$query = "UPDATE newsAlerts SET newsOrder = ? WHERE newsGameId = ? AND newsOrder = ?";
-$preparedQuery = $db->prepare($query);
 $preparedQuery->bind_param("iii", $old2order, $gameId, $tempOrder);
 $preparedQuery->execute();
 
