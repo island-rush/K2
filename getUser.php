f4cf81c7637bbe5c5283a3d148f5a0f7ae9ae98d
spenceradolph <c19spencer.adolph@usafa.edu>
2019-04-23 16:03:59 +0000

    removed weird code from an unrelated proejct, not sure how it got in there...

--- getUser.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$userId = (int) $_GET['userId'];
-
-$allUsers = array("{id:0, name: 'Spencer'}", "{id:1, name: 'Bailey'}", "{id:2, name: 'Adam'}", "{id:3, name: 'Hayze'}", "{id:4, name: 'Someone'}", "{id:5, name: 'Kenobi'}");
-
-echo $allUsers[$userId];
-
-exit;
